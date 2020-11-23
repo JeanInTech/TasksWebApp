3cad752052c41c6a417c8af338836c904b001403
Jean Thomas <jeniferjeanthomas@gmail.com>
2020-11-23 09:05:12 -0500

    Uploading any changes

--- ToDoApp/wwwroot/js/site.js
+++ ToDoApp/wwwroot/js/site.js
@@ -1,17 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-function myFunction() {
+﻿function searchByDescription() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("SearchByDescription");
     filter = input.value.toUpperCase();
     table = document.getElementById("TaskTable");
     tr = table.getElementsByTagName("tr");
 
-    // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[3];
+        td = tr[i].getElementsByTagName("td")[4];
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
